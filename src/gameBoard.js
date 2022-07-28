572b0cbedca469352dbcf08370a34f34c7b84a9f
ThiagoJoseSousa <imigrationstudy@gmail.com>
2022-07-28 11:10:48 -0300

    Time to implement the checkers

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -8,22 +8,49 @@ const gameBoards = () => {
   const getBoard = () => {
     return board;
   };
+
   const spaceChecker= (builtShip, xPlace, yPlace) => {
     const x= xPlace;
     const y= yPlace;
     const length=builtShip.lengthStatus;
 
     let canBePlaced = false;
-    if (length+y<11) {
+    if (length+y<11 && checkForShips(builtShip, xPlace, yPlace)) {
       canBePlaced=true;
     }
     return canBePlaced;
   };
 
-  const positionShip= (x, y) =>{
-    board[x][y] ='O';
+
+  const checkForShips =(builtShip, xPlace, yPlace) =>{
+    const x=xPlace;
+    let y=yPlace;
+    const length=builtShip.lengthStatus;
+    let canBePlaced= false;
+    for (let i=0; i<length; i++) {
+      if (board[x][y]==='') {
+        return canBePlaced=true;
+      } else {
+        canBePlaced=false;
+        break;
+      }
+      y++;
+    }
+    return canBePlaced;
   };
-  return {getBoard, positionShip, spaceChecker};
-};
+
+  for (let row=0; row<board.length; row++) { // undefined cant be referenced
+    for (let column=0; column<board[row].length; column++) {
+      board[row][column] = '';
+    }
+  } // the outter loop wont end until the inner ends
+
+
+  const positionShip= (builtShip, x, y) =>{
+    board[x][y]='O';
+  };
+  return {getBoard, positionShip, spaceChecker, checkForShips};
+}
+;
 
 module.exports= gameBoards;
--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -2,7 +2,7 @@ const gameBoards= require('../src/gameBoard.js');
 
 test('gameBoards is able to place ships at specific coordinates', () => {
   const board = gameBoards();
-  board.positionShip(5, 5);
+  board.positionShip(board, 5, 5);
   const updatedBoard = board.getBoard();
   expect(updatedBoard[5][5]).toMatch('O');
 });
